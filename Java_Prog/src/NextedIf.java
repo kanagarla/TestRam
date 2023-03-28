c3e1d7595bd57237033376c5a79b8bd15b54c3ed
kanag <kanag@LAPTOP-9AHIL9JI>
2023-03-28 07:23:24 +0530

    My First Code

---
+++ Java_Prog/src/NextedIf.java
@@ -0,0 +1,18 @@
+
+public class NextedIf {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int marks=25;
+
+		if(marks>=35)
+		{
+			System.out.println("Pass");
+		}
+		else 
+		{
+						System.out.println("Fail");
+		}
+	}
+
+}
